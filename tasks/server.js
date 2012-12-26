a31764541a978d1b7623be59122ef64c71c22952
Adam Kliment <adam.kliment@virtualmaster.cz>
2012-12-27 00:18:39 +0100

    CORS headers to all requests

--- tasks/server.js
+++ tasks/server.js
@@ -14,6 +14,7 @@ module.exports = function(grunt) {
 
   // External libs.
   var connect = require('connect');
+  var cors = require('connect-xcors')
 
   // ==========================================================================
   // TASKS
@@ -23,24 +24,39 @@ module.exports = function(grunt) {
     // Get values from config, or use defaults.
     var port = grunt.config('server.port') || 8000;
     var base = path.resolve(grunt.config('server.base') || '.');
+    var server
 
-    var middleware = [
-      // Serve static files.
-      connect.static(base),
-      // Make empty directories browsable. (overkill?)
-      connect.directory(base)
-    ];
-
-    // If --debug was specified, enable logging.
+    server = connect()
+    
+    use_cors = true
+    //use_cors = grunt.config('server.cors')
+    if (use_cors) {
+      server.use(function(req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,HEAD,OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type');
+        if(req.method == "OPTIONS"){
+          res.setHeader('Content-Type', 'text/plain charset=UTF-8');
+          res.setHeader('Content-Length', '0');
+          res.setHeader('Access-Control-Max-Age', '1728000');
+          next();
+        } else {
+          next();
+        }
+      })
+    }
+    
     if (grunt.option('debug')) {
       connect.logger.format('grunt', ('[D] server :method :url :status ' +
         ':res[content-length] - :response-time ms').magenta);
-      middleware.unshift(connect.logger('grunt'));
+      server.use(connect.logger('grunt'));
     }
-
-    // Start server.
+    
+    server.use(connect.static(base))
+    server.use(connect.directory(base))
+    
     grunt.log.writeln('Starting static web server on port ' + port + '.');
-    connect.apply(null, middleware).listen(port);
+    server.listen(port)
   });
 
 };
